f5db2a6f880e9e2aa1d54d3e5bdeb73820f6ad69
sifatullahsu <personal.sifat@gmail.com>
2022-11-09 12:25:16 +0600

    mongodb connection & sevices api

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const cluster = process.env.DB_CLUSTER;
+
+
+const uri = `mongodb+srv://${user}:${pass}@${cluster}/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+
+const run = async () => {
+  try {
+    const db = client.db('corner-advisor');
+    const serviceCollection = db.collection('services');
+
+    app.get('/services', async (req, res) => {
+      const page = parseInt(req.query.page) || 1;
+      const size = parseInt(req.query.size) || 10;
+      const skip = (page - 1) * size;
+
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.skip(skip).limit(size).toArray();
+
+      const totalRecord = await serviceCollection.estimatedDocumentCount();
+      const total = Math.ceil(totalRecord / size);
+
+      const data = {
+        data: services,
+        pagination: {
+          total,
+          current: page,
+        }
+      }
+
+      res.send(data);
+    });
+
+
+  }
+  finally {
+
+  }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
