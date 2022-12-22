272af93379bc9ddbe6e1a06dd4de47ecf0490239
sifatullahsu <personal.sifat@gmail.com>
2022-12-22 11:26:51 +0600

    review pagination

--- index.js
+++ index.js
@@ -114,12 +114,23 @@ const run = async () => {
     app.get('/get-reviews-by-email/', verifyJWT, async (req, res) => {
       const email = req.query.email;
 
+      const page = parseInt(req.query.page) || 1;
+      const size = parseInt(req.query.size) || 2;
+      const skip = (page - 1) * size;
+
       const query = { "author.email": email }
       const cursor = reviewCollection.find(query).sort({ date: -1 });
-      const reviews = await cursor.toArray();
+      const reviews = await cursor.skip(skip).limit(size).toArray();
+
+      const totalRecord = await reviewCollection.countDocuments(query);
+      const total = Math.ceil(totalRecord / size);
 
       const data = {
-        data: reviews
+        data: reviews,
+        pagination: {
+          total,
+          current: page,
+        }
       }
 
       res.send(data);
